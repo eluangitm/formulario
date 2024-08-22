ad1983c1d90e84b3bbf23c1d1f4bdf6d9535ec9d
Eluan Rocha <eluanrocha@gmail.com>
2024-08-22 17:38:04 -0300

    adding function

---
+++ formulario/formulario.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form-numero');
+
+function validaResultado(numeroMaiorQueA) {
+    if 'Campo A'>'Campo B'
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+})
+
+console.log(form)
